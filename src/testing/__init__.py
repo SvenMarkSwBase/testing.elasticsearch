8a7f94d25b2ca85cf996708532e8ef7d85a836ce
Sven Mark Hofstra <svenmark10@gmail.com>
2017-07-28 15:43:29 +0200

    Elastic 5.x fix + init to win it 1

--- src/testing/__init__.py
+++ src/testing/__init__.py
@@ -13,5 +13,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__import__('pkg_resources').declare_namespace(__name__)
+# __import__('pkg_resources').declare_namespace(__name__)
 #
